33203e51c9cf29a72da2d7e7e943d87c5d5f2991
eduardo <eduardobarreto19@gmail.com>
2020-06-16 01:10:36 -0500

    Create class "Main Class". The function of this class is to start the program and management the classes and objects

---
+++ src/main/java/ns4/view/MainClass.java
@@ -0,0 +1,58 @@
+package ns4.view;
+
+import n4s.controller.LogicDrones;
+
+/**
+ * 
+ * @author Eduardo
+ * The function of this class is to start the program and management the classes and objects
+ */
+public class MainClass {
+
+	/**
+	 * Relations between classes
+	 */
+	private FileManagement file;
+	private LogicDrones logica;
+	
+	
+	/**
+	 * Constructor MainClass
+	 */
+	public MainClass() {
+		this.file = new FileManagement(this);
+		this.logica = new LogicDrones();
+	}
+
+
+	
+	/**
+	 * Relation with Class FileManagement
+	 * @return Class FileManagement with the object file
+	 */
+	public FileManagement getClasFiles() {
+		return file;
+	}
+
+	
+	
+	/**
+	 * Relation with Class LogicDrones
+	 * @return Class LogicDrones with the object logica
+	 */
+	public LogicDrones getClasLogica() {
+		return logica;
+	}
+	
+	
+
+	/**
+	 * The main method of All Program
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MainClass objetoPrincipal = new MainClass();
+		objetoPrincipal.getClasFiles().readDeliveryRoutes();
+	}
+
+}
